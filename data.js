b442d62861c0e3e6806bcd32d8dbbab23eb028fd
JESSICAMAMABA <mamabajessica@gmail.com>
2025-05-27 14:23:09 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,282 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-tour-1",
+      "name": "LIVING TOUR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6727307274225858,
+          "pitch": 0.017466955278734986,
+          "rotation": 4.71238898038469,
+          "target": "1-kitchen-tour-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen-tour-2",
+      "name": "KITCHEN TOUR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.141152059210775,
+          "pitch": 0.08215480247391405,
+          "rotation": 1.5707963267948966,
+          "target": "2-bathroom-tour-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bathroom-tour-1",
+      "name": "BATHROOM TOUR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.341851204496912,
+          "pitch": 0.08103527569676139,
+          "rotation": 0,
+          "target": "3-bathroom-tour-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bathroom-tour-2",
+      "name": "BATHROOM TOUR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1209802121807666,
+          "pitch": 0.02597056759483074,
+          "rotation": 4.71238898038469,
+          "target": "4-kitchen-tour-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kitchen-tour-1",
+      "name": "KITCHEN TOUR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47162259438784027,
+          "pitch": 0.08930306131864008,
+          "rotation": 10.995574287564278,
+          "target": "5-bedroom-tour-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bedroom-tour-1",
+      "name": "BEDROOM TOUR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40504841161122584,
+          "pitch": 0.07168201657939832,
+          "rotation": 0,
+          "target": "3-bathroom-tour-2"
+        },
+        {
+          "yaw": -0.48103262399905944,
+          "pitch": 0.21465438891831923,
+          "rotation": 4.71238898038469,
+          "target": "6-balcony-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-balcony-1",
+      "name": "BALCONY 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2862705440541262,
+          "pitch": 0.23507140331602372,
+          "rotation": 4.71238898038469,
+          "target": "7-bedroom-tour-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bedroom-tour-2",
+      "name": "BEDROOM TOUR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "APARTMENT CLICKTHROUGH",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
